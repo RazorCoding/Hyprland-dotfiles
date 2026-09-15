8a9e9d4803372ec2df0bad4c9b53b2a47dac746f
RazorCoding <4154540+RazorCoding@users.noreply.github.com>
2026-09-15 16:23:00 +0530

    Update dotfiles

--- .config/hypr/scripts/wob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SOCK="/tmp/wob.sock"
-
-# recreate socket safely
-[ -e "$SOCK" ] && rm "$SOCK"
-mkfifo "$SOCK"
-
-tail -f "$SOCK" | wob
--- .config/waybar/scripts/airplane_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if rfkill list | grep -q "Soft blocked: yes"; then
-    printf '{"text":"󰀝 ","tooltip":"Airplane mode ON"}\n'
-else
-    printf '{"text":"󰀝 ","tooltip":"Airplane mode OFF"}\n'
-fi
-
